329dd186cf16d73dc2e7c2edfc7c4d03731f3dcc
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-02 20:20:36 -0300

    fdsf

--- commands/utility/command.js
+++ commands/utility/command.js
@@ -1,21 +1,21 @@
 module.exports = ({
     name: "command",
     aliases: ['cmd'],
-    description: "Know information about a specific command",
+    description: "Ver la informacion de un comando",
     usage: "command <command name>",
     category: "utility",
     code:
     `
     $title[$toLocaleUppercase[$commandInfo[$message[1];name]]]
-    $description[<> - Mandatory arguments
-() - Optional arguments]
+    $description[<> - Argumentos obligatorios
+() - Argumentos opcionales]
     $color[RANDOM]
-    $addField[Usage;$replaceText[$replaceText[$checkCondition[$commandInfo[$message[1];usage]!=];true;$commandInfo[$message[1];usage]];false;None];yes]
-    $addField[Aliases;$replaceText[$replaceText[$checkCondition[$commandInfo[$message[1];aliases]!=];true;$replaceText[$commandInfo[$message[1];aliases];,;, ]];false;None]]
-    $addField[Description;$replaceText[$replaceText[$checkCondition[$commandInfo[$message[1];description]!=];true;$commandInfo[$message[1];description]];false;None]]
-    $addField[Category;$toLocaleUpperCase[$replaceText[$replaceText[$checkCondition[$commandInfo[$message[1];category]!=];true;$commandInfo[$message[1];category]];false;None]];yes]
-    $onlyIf[$commandInfo[$message[1];name]!=;{description:❌ That is not a valid command!}{color:RANDOM}]
-    $onlyIf[$message!=;{description:You have to specify a command name to see information!}{color:RANDOM}]
+    $addField[Uso;$replaceText[$replaceText[$checkCondition[$commandInfo[$message[1];usage]!=];true;$commandInfo[$message[1];usage]];false;None];yes]
+    $addField[Alias;$replaceText[$replaceText[$checkCondition[$commandInfo[$message[1];aliases]!=];true;$replaceText[$commandInfo[$message[1];aliases];,;, ]];false;None]]
+    $addField[Descripcion;$replaceText[$replaceText[$checkCondition[$commandInfo[$message[1];description]!=];true;$commandInfo[$message[1];description]];false;None]]
+    $addField[Categoria;$toLocaleUpperCase[$replaceText[$replaceText[$checkCondition[$commandInfo[$message[1];category]!=];true;$commandInfo[$message[1];category]];false;None]];yes]
+    $onlyIf[$commandInfo[$message[1];name]!=;{description:❌ Ese no es un comando valido!}{color:RANDOM}]
+    $onlyIf[$message!=;{description:Tienes que especificar un nombre de comando para ver información!}{color:RANDOM}]
     $addTimestamp
     $cooldown[5s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}{delete:$getCooldownTime[globalUser;commandinfo;$authorID;yes]ms}]
     `
