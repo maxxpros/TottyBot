fe906a2dd2005bdccf8764dc0309ac2f5e6fdacd
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-04 09:02:54 -0300

    GSDG

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -4,5 +4,10 @@ module.exports = ({
     usage: "",
     category: "fun",
     code: `$color[RANDOM]
-    $description[<@$authorID> Pregunto $message $random[Si;No;Tal vez;Claro;Imposible]!]`
+    $description[<@$authorID> Pregunto $message $random[Si;No;TalVez;Claro;Imposible]!]
+    $onlyIf[$isNumber[$message[1]]==true;{description: Argument must be a number!}{color:RANDOM}]
+    $onlyIf[$message[1]!=;{description: Tienes que especificar la pregunta!}{color:RANDOM}]
+    $onlyIf[$checkContains[$channelType;text;news]==true;]
+    $suppressErrors[{description: Haci se usa: $getServerVar[prefix]8ball <preguta>}{color:RANDOM}]
+    `
 })
