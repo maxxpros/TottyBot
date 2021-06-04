3186b88dcb4230196201e030477fa3136d7207e5
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-04 09:47:11 -0300

    SGGD

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -4,7 +4,7 @@ module.exports = ({
     usage: "",
     category: "fun",
     code: `$color[RANDOM]
-    $title[$message!]
+    $title[<@$findMember[$message]> Pregunto $message es verdad? $random[10;500]!]
     $onlyIf[$message[1]!=;{description: Tienes que especificar la pregunta!}{color:RANDOM}]
     $suppressErrors[{description: Asi se usa: $getServerVar[prefix]8ball <pregunta>}{color:RANDOM}]
     $onlyIf[$checkContains[$channelType;text;news]==true;]
