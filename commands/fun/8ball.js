27a4bdefc408dc643d36083b1df3e10e33e2ebb0
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-04 09:33:54 -0300

    SGDFG

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -4,7 +4,7 @@ module.exports = ({
     usage: "",
     category: "fun",
     code: `$color[RANDOM]
-    $title[<@$authorID> Pregunto $message[1] $random[Si;No;TalVez;Claro;Imposible]!]
+    $title[<@$authorID> Pregunto $message[1] $random[Si;No]!]
     $onlyIf[$message[1]!=;{description: Tienes que especificar la pregunta!}{color:RANDOM}]
     $suppressErrors[{description: Asi se usa: $getServerVar[prefix]8ball <pregunta>}{color:RANDOM}]
     `
