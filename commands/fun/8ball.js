e773abb8128ed78ae868355191a43a863d5c805a
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-04 08:48:37 -0300

    GFSDG

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,19 +1,20 @@
 module.exports = ({
-    name: "invite",
-    aliases: ['add'],
-    description: "Invitarme a mi server!",
+    name: "8ball",
+    description: "Una pregunta al bot!",
     usage: "",
-    category: "utility",
+    category: "fun",
     code: `
-        $var rpts = ["SÃ­", "No", "Tal vez", "No sÃ©", "Â¡Claro!", "Si <3", "No >:("]; // Las Respuestas
+    if (command === "8ball") { //command Definido
+        var rpts = ["SÃ­", "No", "Tal vez", "No sÃ©", "Â¡Claro!", "Si <3", "No >:("]; // Las Respuestas
     
-        $let pregunt = args.slice(1).join(' '); //Si falta la pregunta
-        $if(!pregunt) return message.channel.send(':x: | Falta la pregunta.');
+        let pregunt = args.slice(1).join(' '); //Si falta la pregunta
+        if(!pregunt) return message.channel.send(':x: | Falta la pregunta.');
         
-
-            $setTitle('Command | 8Ball')
-            $setThumbnail(message.author.avatarURL)
-            $addField('Su pregunta es:', args, true)
-            $addField('Mi respuesta es:', rpts[Math.floor(Math.random() * rpts.length)], true)
-        $message.channel.send(embed)`
+        const embed = new Discord.RichEmbed() //Creamos el embed
+            .setTitle('Command | 8Ball')
+            .setThumbnail(message.author.avatarURL)
+            .addField('Su pregunta es:', args, true)
+            .addField('Mi respuesta es:', rpts[Math.floor(Math.random() * rpts.length)], true)
+        message.channel.send(embed)
+    }`
 })
