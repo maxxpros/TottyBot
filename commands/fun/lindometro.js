4c5dae65c85202fb6f01bbdaf77ce6957e5b7802
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-04 10:11:30 -0300

    SGDG

--- commands/fun/lindometro.js
+++ commands/fun/lindometro.js
@@ -4,7 +4,7 @@ module.exports = ({
     usage: "",
     category: "fun",
     code: `$color[RANDOM]
-    $description[El nivel de lindura de <@$findMember[$message]> es es:
+    $description[El nivel de lindura de <@$findMember[$message]> es de:
     :sunglasses: $random[0;100]% :sunglasses:!]
     $suppressErrors[{description: Asi se usa: $getServerVar[prefix]lindometro}{color:RANDOM}]
     $onlyIf[$checkContains[$channelType;text;news]==true;]
