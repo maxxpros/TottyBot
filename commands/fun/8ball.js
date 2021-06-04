bff5b3a603e687c1f6f533cdbfb7f3f874e40b37
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-04 09:31:23 -0300

    SDGDSG

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -4,10 +4,8 @@ module.exports = ({
     usage: "",
     category: "fun",
     code: `$color[RANDOM]
-    $author[$userTag[$findMember[$messageSlice[1]]];$userAvatar[$findMember[$messageSlice[1]]]]
-    $description[<@$authorID> Pregunto $message[1] $random[Si;No;TalVez;Claro;Imposible]!]
+    $title[<@$authorID> Pregunto $message[1] $random[Si;No;TalVez;Claro;Imposible]!]
     $onlyIf[$message[1]!=;{description: Tienes que especificar la pregunta!}{color:RANDOM}]
-    $onlyIf[$checkContains[$channelType;text;news]==true;]
-
+    $suppressErrors[{description: Asi se usa: $getServerVar[prefix]8ball <pregunta>}{color:RANDOM}]
     `
 })
