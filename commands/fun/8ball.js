f99e163d75fb42373c7babfb1e5e8bddca192a58
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-04 08:55:09 -0300

    GFDG

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -3,18 +3,6 @@ module.exports = ({
     description: "Una pregunta al bot!",
     usage: "",
     category: "fun",
-    code: `
-    if (command === "8ball") { //command Definido
-        var rpts = ["SÃ­", "No", "Tal vez", "No sÃ©", "Â¡Claro!", "Si <3", "No >:("]; // Las Respuestas
-    
-        let pregunt = args.slice(1).join(' '); //Si falta la pregunta
-        if(!pregunt) return message.channel.send(':x: | Falta la pregunta.');
-        
-        const embed = new Discord.RichEmbed() //Creamos el embed
-            .setTitle('Command | 8Ball')
-            .setThumbnail(message.author.avatarURL)
-            .addField('Su pregunta es:', args, true)
-            .addField('Mi respuesta es:', rpts[Math.floor(Math.random() * rpts.length)], true)
-        message.channel.send(embed)
-    }`
+    code: `$color[RANDOM]
+    $description[<@$authorID> Pregunto $message $random[Si;No;Tal vez;Claro;Imposible]!]`
 })
