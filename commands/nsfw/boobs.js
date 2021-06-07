e08e512c448f2cb988f915f760dace776119f3a5
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-06 22:29:40 -0300

    DSGSD

--- commands/nsfw/boobs.js
+++ commands/nsfw/boobs.js
@@ -1,16 +1,22 @@
 module.exports = ({
     name: "boobs",
-    aliases: ['dead'],
-    usage: "rip (user)",
-    description: "⚰",
     category: "fun",
-    code: `
-    const Discord = require("discord.js");
-    const NSFW = require("discord-nsfw");
-    const nsfw = new NSFW();
-
-
-    $color[RANDOM]
-    $image[boobs]
+    description: "Muestra un meme aleatorio de Reddit",
+    usage: "", 
+    code: `$djseval[(async()=>{
+   channel.startTyping()
+   const Discord = require("discord.js");
+   const NSFW = require("discord-nsfw");
+   const nsfw = new NSFW();
+   
+   const image = await nsfw.pussy();
+   const embed = new Discord.MessageEmbed()
+   .setTitle(Boobs)
+   .setColor('RANDOM')
+   .setImage(image)
+   message.channel.send(embed)
+   channel.stopTyping()
+   })()]
+   $cooldown[3s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}]
    $onlyIf[$checkContains[$channelType;text;news]==true;]`
-})
+   })  
