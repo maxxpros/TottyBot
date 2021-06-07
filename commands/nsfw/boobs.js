913080c18872e1dc32a60f20b06a567ebb63711b
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-06 22:55:37 -0300

    GDFGD

--- commands/nsfw/boobs.js
+++ commands/nsfw/boobs.js
@@ -1,12 +1,13 @@
 module.exports = ({
     name: "boobs",
-    category: "nsfw",
+    aliases: ['shoot'],
+    category: "fun",
     description: "Sostener un arma",
     usage: "gun (user)",
     code: `
-    $image[https://api.tomio.codes/api/boobs?url]
+    $image[https://api.tomio.codes/api/boobs?url=$replaceText[$userAvatar[$findUser[$message]];webp;png]]
     $color[RANDOM]
     $addTimestamp
     $cooldown[3s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}]
     $onlyIf[$checkContains[$channelType;text;news]==true;]`
-})
+   })
