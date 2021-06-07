a75ca68fefdd9a38ceb4749355c171f718ac280a
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-06 22:43:20 -0300

    DSFDSF

--- commands/nsfw/boobs.js
+++ commands/nsfw/boobs.js
@@ -1,20 +1,12 @@
 module.exports = ({
     name: "boobs",
-    category: "fun",
-    description: "Muestra un meme aleatorio de Reddit",
-    usage: "", 
+    category: "nsfw",
+    description: "Sostener un arma",
+    usage: "gun (user)",
     code: `
-   const Discord = require("discord.js");
-   const fetch = require('node-fetch')
-   const NSFW = require("discord-nsfw");
-   const nsfw = new NSFW();
-   
-   const image = await nsfw.pussy();
-   const embed = new Discord.MessageEmbed()
-   .setTitle(Boobs2)
-   .setColor('RANDOM')
-   .setImage(image)
-   message.channel.send(embed)
-   $cooldown[3s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}]
-   $onlyIf[$checkContains[$channelType;text;news]==true;]`
-   })  
+    $image[https://api.tomio.codes/api/boobs]
+    $color[RANDOM]
+    $addTimestamp
+    $cooldown[3s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}]
+    $onlyIf[$checkContains[$channelType;text;news]==true;]`
+})
