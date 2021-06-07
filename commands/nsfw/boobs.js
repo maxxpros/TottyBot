499b7af9289c90154af36f2dae7be546c3e9219f
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-06 23:35:46 -0300

    FSFSD

--- commands/nsfw/boobs.js
+++ commands/nsfw/boobs.js
@@ -1,13 +1,13 @@
 module.exports = ({
     name: "boobs",
-    aliases: ['shoot'],
+    description: "Una pregunta al bot!",
+    usage: "8ball <pregunta>",
     category: "fun",
-    description: "Sostener un arma",
-    usage: "gun (user)",
-    code: `
-    $tittle[https://api.tomio.codes/api/email?message]
-    $color[RANDOM]
-    $addTimestamp
-    $cooldown[3s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}]
-    $onlyIf[$checkContains[$channelType;text;news]==true;]`
-   })
+    code: `$color[RANDOM]
+    $description[<@$findMember[$message]> Queria ver unas $message]
+    $image[$randomText[https://cdn.sex.com/images/pinporn/2019/10/14/21982884.gif?width=620;https://cdn.sex.com/images/pinporn/2019/10/14/21982878.gif?width=620;https://cdn.sex.com/images/pinporn/2015/12/12/14524830.jpg?width=620&site=sex&user=rika-chan;https://cdn.sex.com/images/pinporn/2018/03/22/19277821.jpg?width=620&site=sex&user=zokix;https://cdn.sex.com/images/pinporn/2019/09/14/21811770.jpg?width=620&site=sex&user=eric2199;https://cdn.sex.com/images/pinporn/2020/01/21/22532723.jpg?width=620&site=sex&user=ming99;https://cdn.sex.com/images/pinporn/2017/07/11/18036173.jpg?width=620&site=sex&user=hotgirlhub]!]
+    $onlyIf[$message[1]!=;{description: Tienes que especificar la pregunta!}{color:RANDOM}]
+    $suppressErrors[{description: Asi se usa: $getServerVar[prefix]8ball <pregunta>}{color:RANDOM}]
+    $onlyIf[$checkContains[$channelType;text;news]==true;]
+    `
+})
