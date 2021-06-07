2ca701e71b609a9db8f7b59fc8bdd200d24c7452
Tomas Carbone <tomas@DESKTOP-5063.5063>
2021-06-06 23:59:15 -0300

    FSFS

---
+++ commands/utility/covid.js
@@ -0,0 +1,10 @@
+module.exports = ({
+    name: "covid",
+    aliases: ['pag'],
+    description: "Pagina del bot!",
+    usage: "",
+    category: "utility",
+    code: `
+    $description[https://dinosaur.ml/covid19/country/?country=$message]
+    $color[RANDOM]`
+})
